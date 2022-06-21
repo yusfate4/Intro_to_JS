9eeaec799bfa83e89f1cf4ddfa4cabd7047e409d
Dahud Yusuf <abiodunyusuf4@gmail.com>
2022-06-21 23:43:03 +0100

    Introduction to Javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+var name = "Dahud Yusuf";
+      console.log("My name is " + name);
+      alert("my name is " + name);
+      var height = "5'9";
+      console.log("I am " + height);
+      alert("I am " + height);
+      var country = "Nigeria";
+      console.log("The name of my country is " + country);
+      alert("The name of my country is " + country);
